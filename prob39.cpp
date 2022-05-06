f206e14facc9a110048aab5233bf5c44a469675e
TinNguyen <ductin16111@gmail.com>
2022-05-06 22:49:31 +0300

    add code comment explanations

--- prob39.cpp
+++ prob39.cpp
@@ -12,24 +12,29 @@ int main(void){
     for (int j = 1; j<1000; j++){
       for (int k = 1; k<1000; k++){
         if((i+j+k<=1000) && (i*i + j*j == k*k)){
-          //cout << i << " " << j << " " << k << endl;
-          sum[count++] = i+j+k;
-          //count++;
+            //At first I list all Pythagoras solutions under 1000 with sum <= 1000 and count how many of them
+            //cout << i << " " << j << " " << k << endl;
+            //count++;
+            //I found that there are 650 solutions, 
+            //so I set len = 650 and declare an array sum to store the solutions' sums
+            sum[count++] = i+j+k;   
         }
       }
     }
   }
   //cout << count;
   sort(sum, sum+count);
-  int index = 1;
+  //since sum[] is sorted, all identical sums will be grouped consecutively and in ascending order
+  //for ex [sum1, sum1, sum2, sum2, sum2, sum3, sum3, sum4, sum5, ...]
+  int index = 1;    //count the number of times a sum appear in the sum[] array
   int max_index = 1, p;
   for(int i = 0; i<len; i++){
     //cout << sum[i] << " " << index << endl;
-    if(sum[i] == sum[i+1]) index++;
-    else index = 1;
+    if(sum[i] == sum[i+1]) index++;     //if a sum appears 4 times, 4 consecutives are equal and index += 4 
+    else index = 1;                     //index is restarted when a different sum appears
     if(index > max_index)  {
-      max_index = index;
-      p = sum[i];
+      max_index = index;    //found the maximum number of times a sum appears
+      p = sum[i];           //and store that sum to p
     }
   }
   cout << p;
