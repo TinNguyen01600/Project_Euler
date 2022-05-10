817371ef2878ffc27618be2c3153412df34775ef
TinNguyen <ductin16111@gmail.com>
2022-05-10 23:31:29 +0300

    add function check() to check if the number n equal to its digits' factorial sum

--- prob34.py
+++ prob34.py
@@ -10,6 +10,20 @@ def fac(n):
     else:
         return (n * fac(n-1))
     
+def check(n):
+    temp = n
+    sum = 0
+    while(n != 0):
+        r = n%10
+        print(fac(r))
+        sum += fac(r)
+        n = int(n/10)
+    if(sum == temp):
+        return 1
+    else:
+        return 0
+    
 if __name__ == '__main__':
     n = int(input("Enter number: "))
-    print(fac(n))
+    #print(fac(n))
+    print(check(n))
