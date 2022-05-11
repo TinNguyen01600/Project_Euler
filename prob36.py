043571adbde0ba22b7c87f2e758f2d17e13e7fba
TinNguyen <ductin16111@gmail.com>
2022-05-11 23:20:30 +0300

    add function check_19() to check if number is palindrome in base 10

--- prob36.py
+++ prob36.py
@@ -15,6 +15,19 @@ def base_10_to_2(num):
       decimalRemainder = math.floor(decimalRemainder / 2) 
     return binaryValue
 
+def check_10(n):    #check if n is palindrome in 10-base
+    temp = n
+    x = 0
+    while(temp != 0):
+        r = temp%10
+        x = x*10 + r
+        temp = int(temp/10)
+    if(x == n):
+        return 1
+    else:
+        return 0
+
 if __name__ == '__main__':
   n = int(input("Enter a number: "))
   print(base_10_to_2(n))
+  print(check_10(n))
