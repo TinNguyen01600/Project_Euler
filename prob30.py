994608b40eeeb3c89e1805dcc41d03c780b693cd
TinNguyen <ductin16111@gmail.com>
2022-05-14 21:07:08 +0300

    add function check() to check if number equals its sum digit fourth powers

--- prob30.py
+++ prob30.py
@@ -5,11 +5,19 @@ import re
 import sys
 import array as arr
 
-if __name__ == '__main__':
-    n = int(input("Enter a number: "))
+def check(n):
+    temp = n
     sum = 0
     while(n!=0):
         r = int(n%10)
         sum += (r**4)
         n = int(n/10)
-    print(sum)
+    if sum == temp:
+        return 1
+    else:
+        return 0
+
+if __name__ == '__main__':
+    n = int(input("Enter a number: "))
+    
+    print(check(n))
