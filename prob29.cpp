706a7222755a0e79aaed16373ab96365ccdc5a63
TinNguyen <ductin16111@gmail.com>
2022-05-17 22:47:45 +0300

    check if the program works with 2<= a <=5 and 2<= b <= 5

--- prob29.cpp
+++ prob29.cpp
@@ -7,13 +7,16 @@ using namespace std;
 int main(void){
     long double arr[16];
     int count = 0;
-    for (long double i = 2; i<5; i++){
-        for(long double j = 2; j<5; j++){
+    for (long double i = 2; i<=5; i++){
+        for(long double j = 2; j<=5; j++){
             arr[count] = pow(i, j);
             count++;
         }
     }
-    for(int i = 0; i<count ; i++){
-        cout << arr[i] << endl;
+    sort(arr, arr+count);
+    int k = count;
+    for(int i = 0; i<count; i++){
+        if(arr[i] == arr[i+1])   k--;
     }
+    cout << k;
 }
