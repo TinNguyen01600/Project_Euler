4dddd17dde6254dfdd0dc461ad10ade256ccc007
TinNguyen <ductin16111@gmail.com>
2022-07-06 17:06:40 +0300

    add problem 16 by C++

---
+++ prob16.cpp
@@ -0,0 +1,12 @@
+#include <algorithm>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main(void){
+    int arr[10], i;
+    arr[9] = 0;
+    for (i = 0; i<9; i++){
+        arr[i] = 0;
+    }
+}
