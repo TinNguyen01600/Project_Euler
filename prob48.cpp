dac07ee6acb61741535c954cf18e88322b14766c
TinNguyen <ductin16111@gmail.com>
2022-07-17 11:52:02 +0300

    code correct with n=23

--- prob48.cpp
+++ prob48.cpp
@@ -8,14 +8,20 @@ using namespace std;
 use long long int), so I store its digits in an array
  */
 
+// with n>10, 10-digit array is incorrect
+// set the number of digit to 300
+const int len = 300;
+
 // This function only calculate and display last 10 digits of a self-power number
 int *self_power(int n){
-    int *result = new int [10];
+    int *result = new int [len];
     int r = 0;
-    for (int i = 0; i<9; i++)   result[i] = 0;
-    result[9] = 1;
+
+    for (int i = 0; i<len-1; i++)   result[i] = 0;
+    result[len-1] = 1;
+    
     for (int j = 1; j <= n; j++){
-        for (int i = 9; i>=0; --i){
+        for (int i = len-1; i>=0; --i){
         result[i] *= n;
         result[i] += r;
         if (result[i] > 9)  {r = result[i]/10; result[i] %= 10;}
@@ -28,7 +34,7 @@ int *self_power(int n){
 // This function calculate sum of last 10 digits of 2 array-numbers
 void plus1(int arr1[], int arr2[], int result[]){
   int n = 0;
-  for (int i = 9; i>=0; --i){
+  for (int i = len-1; i>=(len-10); --i){
     result[i] = arr1[i] + arr2[i] + n;
     if(result[i] > 9)  {result[i] %= 10; n = 1;}
     else n = 0;
@@ -36,23 +42,23 @@ void plus1(int arr1[], int arr2[], int result[]){
 }
 
 int main(void){
-    int *arr1 = new int [10];
-    int *arr2 = new int [10];
-    int *result = new int [10];
+    int *arr1 = new int [len];
+    int *arr2 = new int [len];
+    int *result = new int [len];
     int i, j, k;
 
-    for (int i = 0; i<10; i++)  arr1[i] = 0;
+    for (int i = len-10; i<len; i++)  arr1[i] = 0;
 
     // arr1 = self_power(5);
     // for (int i = 0; i<10; i++)  cout << arr1[i];
     // plus1(arr1, arr2, result);
     // for (int i = 0; i<10; i++)  cout << result[i];
 
-    for(i = 1; i<=10; i++){
-        for (j = 0; j<10; j++)  result[j] = 0;
+    for(i = 1; i<=23; i++){
+        for (j = len-10; j<len; j++)  result[j] = 0;
         arr2 = self_power(i);
         plus1(arr1, arr2, result);
-        for (k = 0; k<10; k++)  arr1[k] = result[k];
+        for (k = len-10; k<len; k++)  arr1[k] = result[k];
     }
-    for (i = 0; i<10; i++)  cout << result[i];
+    for (i = len-10; i<len; i++)  cout << result[i];
 }
