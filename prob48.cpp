be8bb9f81ac82d6aff3d1c6b5b1b36689ad4a949
TinNguyen <ductin16111@gmail.com>
2022-07-17 11:41:40 +0300

    code correct with n=10

--- prob48.cpp
+++ prob48.cpp
@@ -3,6 +3,12 @@
 #include <cmath>
 using namespace std;
 
+/*
+1000^1000 is very big integer that C++ cannot handle (cannot 
+use long long int), so I store its digits in an array
+ */
+
+// This function only calculate and display last 10 digits of a self-power number
 int *self_power(int n){
     int *result = new int [10];
     int r = 0;
@@ -19,6 +25,7 @@ int *self_power(int n){
     return result;
 }
 
+// This function calculate sum of last 10 digits of 2 array-numbers
 void plus1(int arr1[], int arr2[], int result[]){
   int n = 0;
   for (int i = 9; i>=0; --i){
@@ -32,11 +39,20 @@ int main(void){
     int *arr1 = new int [10];
     int *arr2 = new int [10];
     int *result = new int [10];
+    int i, j, k;
 
-    for (int i = 0; i<10; i++)  arr2[i] = 1;
+    for (int i = 0; i<10; i++)  arr1[i] = 0;
 
-    arr1 = self_power(5);
+    // arr1 = self_power(5);
     // for (int i = 0; i<10; i++)  cout << arr1[i];
-    plus1(arr1, arr2, result);
-    for (int i = 0; i<10; i++)  cout << result[i];
+    // plus1(arr1, arr2, result);
+    // for (int i = 0; i<10; i++)  cout << result[i];
+
+    for(i = 1; i<=10; i++){
+        for (j = 0; j<10; j++)  result[j] = 0;
+        arr2 = self_power(i);
+        plus1(arr1, arr2, result);
+        for (k = 0; k<10; k++)  arr1[k] = result[k];
+    }
+    for (i = 0; i<10; i++)  cout << result[i];
 }
