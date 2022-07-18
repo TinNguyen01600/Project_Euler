76f481db07b26560baeac93bf8a1b6db002d26a8
TinNguyen <ductin16111@gmail.com>
2022-07-18 09:43:42 +0300

    initialize 2 arrays to store Fib1 and Fib2

--- prob25.cpp
+++ prob25.cpp
@@ -15,10 +15,20 @@ void plus1(int arr1[], int arr2[], int result[]){
 }
 
 int main(void){
+    int *arr1 = new int [len];
+    int *arr2 = new int [len];
     int *result = new int [len];
     int i, j, k;
-    int arr1[3] = {9, 9, 9};
-    int arr2[3] = {5, 5, 5};
-    plus1(arr1, arr2, result);
+
+    // int arr1[3] = {9, 9, 9};
+    // int arr2[3] = {5, 5, 5};
+
+    for (i = 0; i<len-1; i++)   arr1[i] = 0;
+    arr1[len-1] = 1;
+    for (i = 0; i<len-1; i++)   arr2[i] = 0;
+    arr2[len-1] = 1;
+    result[0] = 0;
+    
+    // plus1(arr1, arr2, result);
     for (i = 0; i<len; i++)  cout << result[i];
 }
