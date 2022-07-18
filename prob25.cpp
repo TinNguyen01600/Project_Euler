223a68025f83c0466b4b344dc46f97e4b084876c
TinNguyen <ductin16111@gmail.com>
2022-07-18 14:34:02 +0300

    complete problem 25

--- prob25.cpp
+++ prob25.cpp
@@ -3,7 +3,7 @@
 #include <cmath>
 using namespace std;
 
-const int len = 3;
+const int len = 1000;
 
 void plus1(int arr1[], int arr2[], int result[]){
   int n = 0;
@@ -18,7 +18,7 @@ int main(void){
     int *arr1 = new int [len];
     int *arr2 = new int [len];
     int *result = new int [len];
-    int i, j, k;
+    int i, j, k, count = 2;
 
     // int arr1[3] = {9, 9, 9};
     // int arr2[3] = {5, 5, 5};
@@ -28,7 +28,15 @@ int main(void){
     for (i = 0; i<len-1; i++)   arr2[i] = 0;
     arr2[len-1] = 1;
     result[0] = 0;
-    
+
+    while(result[0] == 0){
+        for (i = 0; i<len; i++)   result[i] = 0;
+        plus1(arr1, arr2, result);
+        for (j = 0; j<len; j++)   arr1[j] = arr2[j];
+        for (k = 0; k<len; k++)   arr2[k] = result[k];
+        count++;
+    }
     // plus1(arr1, arr2, result);
-    for (i = 0; i<len; i++)  cout << result[i];
+    // for (i = 0; i<len; i++)  cout << result[i];
+    cout << endl << count;
 }
