ec678bb272d231c0d585de6d42e3657679be6cd2
TinNguyen <ductin16111@gmail.com>
2022-07-23 17:24:15 +0300

    complete problem 24

--- prob24.py
+++ prob24.py
@@ -1,5 +1,8 @@
 from itertools import permutations 
-perm = permutations([0, 1, 2])
+perm = permutations([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
 
+i = 0
 for val in perm:
-    print(*val)
+    i+=1
+    if i == 1000000:
+        print(*val)
